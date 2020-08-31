a2103f1627a9b42fcbf5f3b1e12a9a04182f5acc
07sahilg <44223359+07sahilg@users.noreply.github.com>
2020-08-31 10:00:40 +0530

    Exception Handling

--- src/main/java/com/example/shows/demo/Exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/shows/demo/Exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import javax.jws.WebResult;
 import java.util.Date;
 
