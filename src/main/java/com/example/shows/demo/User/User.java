80b8974db4b015c20dc729b8bec01f69581ac7b1
07sahilg <44223359+07sahilg@users.noreply.github.com>
2020-08-31 09:58:12 +0530

    Update User.java

--- src/main/java/com/example/shows/demo/User/User.java
+++ src/main/java/com/example/shows/demo/User/User.java
@@ -24,17 +24,14 @@ public class User {
 	@Digits(fraction = 0,integer = 7,message = "Phone should be less than 8 digits")
 	private Integer phnNum;
 
-	 // date should be in past
+	@Future// date should be in past
 	private Date showTime;
 
 	private boolean expire;
+	
+	User()
+	{
 
-	public boolean isExpire() {
-		return expire;
-	}
-
-	public void setExpire(boolean expire) {
-		this.expire = expire;
 	}
 
 	public User(Integer id, String name, Date showTime, Integer phnNum, boolean expire) {
@@ -44,11 +41,13 @@ public class User {
 		this.phnNum = phnNum;
 		this.expire = expire;
 	}
+	
+	public boolean isExpire() {
+		return expire;
+	}
 
-
-	User()
-	{
-
+	public void setExpire(boolean expire) {
+		this.expire = expire;
 	}
 
 	public Integer getId() {
